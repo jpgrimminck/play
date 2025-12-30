2db49ac62449ab58e73989c5d6a3edc0d0c8f24c
Aliya Salmanova <aliyasalmanova1@gmail.com>
2025-12-30 14:07:36 -0500

    switched back to testing database

--- usuarios/config.js
+++ usuarios/config.js
@@ -1,11 +1,11 @@
 // Public client config for Supabase (read-only usage in the browser)
 
 //for testing: 
-//window.SUPABASE_URL = 'https://gfqexhtehomelqtevpex.supabase.co';
+window.SUPABASE_URL = 'https://gfqexhtehomelqtevpex.supabase.co';
 //for production: 
-window.SUPABASE_URL = 'https://pksschwcslljwrsmwhtd.supabase.co';
+//window.SUPABASE_URL = 'https://pksschwcslljwrsmwhtd.supabase.co';
 
 //for testing: 
-//window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdmcWV4aHRlaG9tZWxxdGV2cGV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM1OTA0MzQsImV4cCI6MjA2OTE2NjQzNH0.x7-lBYEOq16DDLF7TOZjuh6g3ZfRrAMc6SqG8MSPmQE';
+window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdmcWV4aHRlaG9tZWxxdGV2cGV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM1OTA0MzQsImV4cCI6MjA2OTE2NjQzNH0.x7-lBYEOq16DDLF7TOZjuh6g3ZfRrAMc6SqG8MSPmQE';
 //for production: 
-window.SUPABASE_ANON_KEY  = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBrc3NjaHdjc2xsandyc213aHRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY0MDkzMjMsImV4cCI6MjA4MTk4NTMyM30.RedXSjP4XlGuMY-1FHiCGKsH1_B0DN2YEPenN4S9pF8';
+//window.SUPABASE_ANON_KEY  = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBrc3NjaHdjc2xsandyc213aHRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY0MDkzMjMsImV4cCI6MjA4MTk4NTMyM30.RedXSjP4XlGuMY-1FHiCGKsH1_B0DN2YEPenN4S9pF8';
